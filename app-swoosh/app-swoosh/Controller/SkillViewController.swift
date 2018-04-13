2511f00ccbc68ecb5b4bf980ded64d68f4348071
Gary Kort <gary@globality.nl>
2018-04-13 21:33:42 +0200

    last lession

--- app-swoosh/app-swoosh/Controller/SkillViewController.swift
+++ app-swoosh/app-swoosh/Controller/SkillViewController.swift
@@ -21,7 +21,6 @@ class SkillViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
     /*
     // MARK: - Navigation
 
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Gary Kort on 13-04-18.
+//  Copyright © 2018 Globality. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
